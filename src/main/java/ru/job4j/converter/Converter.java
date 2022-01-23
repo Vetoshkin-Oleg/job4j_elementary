5d41adfec1cb28fc1fe1f06c0af8c5920b483b05
Oleg <83689064+Vetoshkin-Oleg@users.noreply.github.com>
2022-01-23 14:50:43 +0500

    Update Converter.java

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -1,28 +1,20 @@
 package ru.job4j.converter;
 
-public class Converter {
+import org.junit.Assert;
+import org.junit.Test;
 
-    public static float rubleToEuro(float value) {
-        float rsl = value / 70;
-        return rsl;
+public class ConverterTest {
+
+    @Test
+    public void whenConvert140RblThen2Euro() {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
     }
 
-    public static float rubleToDollar(float value) {
-        float rsl = value / 60; /* формула перевода рублей в доллары. */
-        return rsl;
-    }
-
-    public static void main(String[] args) {
-        float inEuro = 140;
-        float expectedEuro = 2;
-        float outEuro = Converter.rubleToEuro(inEuro);
-        boolean passedEuro = expectedEuro == outEuro;
-        System.out.println("140 rubles are 2. Test result : " + passedEuro);
-
-        float inDollar = 180;
-        float expectedDollar = 3;
-        float outDollar = Converter.rubleToDollar(inDollar);
-        boolean passedDollar = expectedDollar == outDollar;
-        System.out.println("180 rubles are 3. Test result : " + passedDollar);
+    @Test
+    public void whenConvert200RblThen3dot3333Dlr() {
     }
 }
