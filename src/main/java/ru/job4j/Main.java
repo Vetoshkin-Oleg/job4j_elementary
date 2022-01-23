b3ffc48605acb43aa602181820c6053d29a57db6
Vetoshkin-Oleg <voa2014@google.com>
2022-01-23 09:57:47 +0500

    Main

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,6 +2,21 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int age = 25;
+        char gender = 'M';
+        float height = 1.75F;
+        int numberBricks = 40000;
+        double numberBacteria = 1e107;
+        double distance = 4.6e9;
+        boolean workPermit = true;
+        int size = 5000000;
+        System.out.println(age);
+        System.out.println(gender);
+        System.out.println(height);
+        System.out.println(numberBricks);
+        System.out.println(numberBacteria);
+        System.out.println(distance);
+        System.out.println(workPermit);
+        System.out.println(size);
     }
 }
