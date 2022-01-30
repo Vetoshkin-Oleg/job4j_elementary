48fd8e2f9375841b998bf7db113e6d43abafbb24
Vetoshkin-Oleg <voa2014@google.com>
2022-01-30 18:08:17 +0500

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива surnames равен: " + surnames.length);
         System.out.println("Размер массива prices равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Oleg";
+        names[1] = "Maria";
+        names[2] = "Ivan";
+        names[3] = "Svetlana";
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
