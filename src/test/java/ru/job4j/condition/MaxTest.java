f048a4826f0aa021b3bfc7267521e5ba3f2f9d46
Vetoshkin-Oleg <voa2014@google.com>
2022-02-18 00:22:41 +0500

    3. Перегрузить метод max для трех чисел [#122644]

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -31,4 +31,78 @@ public class MaxTest {
         int expected = 2;
         Assert.assertEquals(result, expected);
     }
+
+    @Test
+    public void whenMax1And2And3Then3() {
+        int first = 1;
+        int second = 2;
+        int third = 3;
+        int result = Max.max(first, second, third);
+        int expected = 3;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax1And3And2Then3() {
+        int first = 1;
+        int second = 3;
+        int third = 2;
+        int result = Max.max(first, second, third);
+        int expected = 3;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax3And2And1Then3() {
+        int first = 3;
+        int second = 2;
+        int third = 1;
+        int result = Max.max(first, second, third);
+        int expected = 3;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax1And2And3And4Then4() {
+        int first = 1;
+        int second = 2;
+        int third = 3;
+        int four = 4;
+        int result = Max.max(first, second, third, four);
+        int expected = 4;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax1And3And4And2Then4() {
+        int first = 1;
+        int second = 3;
+        int third = 4;
+        int four = 2;
+        int result = Max.max(first, second, third, four);
+        int expected = 4;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax2And4And3And1Then4() {
+        int first = 2;
+        int second = 4;
+        int third = 3;
+        int four = 1;
+        int result = Max.max(first, second, third, four);
+        int expected = 4;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax4And1And3And2Then4() {
+        int first = 4;
+        int second = 1;
+        int third = 3;
+        int four = 2;
+        int result = Max.max(first, second, third, four);
+        int expected = 4;
+        Assert.assertEquals(result, expected);
+    }
 }
